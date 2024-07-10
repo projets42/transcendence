483cc920694310fc171bc7ad28c4a0e42097f3c1
Sam0611 <samantha_malloire@orange.fr>
2024-07-10 20:35:49 +0200

    one page application

--- django/transcendence/urls.py
+++ django/transcendence/urls.py
@@ -16,11 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
 from userprofiles.views import *
 
 urlpatterns = [
     path('', index),
-    path('page2.html/', page2),
     path('admin/', admin.site.urls),
 ]
--- django/userprofiles/views.py
+++ django/userprofiles/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "index.html")
-
-def page2(request):
-    return render(request, "page2.html")
+    context = {"page": 1}
+    if request.GET.get('play') == 'PLAY':
+        context = {"page": 2}
+    return render(request, "index.html", context)
