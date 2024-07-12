6bf9f6a176448ac8ed65a5ec51bd099f25f99240
Sam0611 <samantha_malloire@orange.fr>
2024-07-12 21:33:17 +0200

    postgresql container created

--- django/transcendence/settings.py
+++ django/transcendence/settings.py
@@ -75,6 +75,17 @@ WSGI_APPLICATION = 'transcendence.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
