8c76fff654e6c567677a1cfcb1ed5fb48f6a7b0c
Sam0611 <samantha_malloire@orange.fr>
2024-07-24 21:35:58 +0200

    bomberman database created

--- django/bomberman/models.py
+++ django/bomberman/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Bomberman(models.Model):
+    winner = models.CharField(max_length = 255)
+    loser = models.CharField(max_length = 255)
+    winner_score = models.IntegerField(default = 0)
+    looser_score = models.IntegerField(default = 0)
+    date = models.DateField()
