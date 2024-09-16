afb75e560a7899e889bf807762f69796351d888f
Sam0611 <samantha_malloire@orange.fr>
2024-09-16 21:26:30 +0200

    get info with access token in a request

--- srcs/app/django/transcendence/views.py
+++ srcs/app/django/transcendence/views.py
@@ -60,6 +60,8 @@ def test(request):
             result = response.json()
             return render(request, "test.html", {"token": result["access_token"]})
 
+        # curl -H "Authorization: Bearer YOUR_ACCESS_TOKEN" "https://api.intra.42.fr/v2/users"
+
         if request.POST.get('auth'):
             url = "https://api.intra.42.fr/oauth/authorize"
             params = {
@@ -72,6 +74,25 @@ def test(request):
             response = requests.get(url, data=params)
             result = response.json()
             return render(request, "test.html", {"token": "ok"})
+
+        if request.POST.get('info'):
+            url = "https://api.intra.42.fr/oauth/token"
+            params = {
+                "grant_type": 'client_credentials',
+                "client_id": 'u-s4t2ud-34134e67a8827ff2a35b81c9dd90550be95e01ca581ca6c3a51f26b8241b983b',
+                "client_secret": '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'
+            }
+            response = requests.post(url, data=params)
+            result = response.json()
+            token = result["access_token"]
+
+            url = "https://api.intra.42.fr/v2/users"
+            headers = {
+                "Authorization": f"Bearer {token}"
+            }
+            response = requests.get(url, headers=headers)
+            result = response.json()
+            return render(request, "test.html", {"token": result})
 
     # print(token.get("/v2/cursus").parsed)
     return render(request, "test.html")
